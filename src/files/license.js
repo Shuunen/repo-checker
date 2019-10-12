f8606e9010541e6f6da799c736b17c1386ce4394
Romain Racamier-Lafon <romain.racamier@gmail.com>
2019-10-12 18:18:44 +0200

    add MIT case

--- src/files/license.js
+++ src/files/license.js
@@ -3,9 +3,14 @@ const Test = require('../test')
 class CheckLicense extends Test {
   async start () {
     await this.checkFile('LICENSE')
-    if (this.data.license === 'GPL-3.0') {
+    const { license } = this.data
+    if (license === 'GPL-3.0') {
       this.shouldContains('a GPL title', /GNU GENERAL PUBLIC LICENSE/)
       this.shouldContains('a version 3 subtitle', /Version 3, 29 June 2007/)
+      return
+    }
+    if (license === 'MIT') {
+      this.shouldContains('a MIT title', /MIT License/)
     }
   }
 }
