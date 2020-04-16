2899ed77df67ae5025eeb606d2fd48f3308bf328
Romain Racamier-Lafon <romain.racamier@gmail.com>
2020-04-16 16:53:58 +0200

    show bundle metrics

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
 import resolve from '@rollup/plugin-node-resolve'
 import shebang from 'rollup-plugin-add-shebang'
+import analyze from 'rollup-plugin-analyzer'
 import pkg from './package.json'
 
 export default [
@@ -21,6 +22,7 @@ export default [
       { file: pkg.main, format: 'cjs' },
     ],
     plugins: [
+      analyze({ summaryOnly: true, limit: 10 }),
       shebang(),
       json(),
       resolve(), // so Rollup can find `ms`
