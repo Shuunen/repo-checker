66b5a9a90ea834f70b2592404939678879e465f0
Romain Racamier-Lafon <romain.racamier@gmail.com>
2020-11-22 12:38:20 +0100

    bump to last Node LTS

--- src/files/nvmrc.js
+++ src/files/nvmrc.js
@@ -5,9 +5,6 @@ export class NvmrcFile extends File {
     const exists = await this.checkFileExists('.nvmrc')
     if (!exists) return
     await this.inspectFile('.nvmrc')
-    if (!this.fileContent.includes('v12.')) {
-      const ok = this.shouldContains('at least last lts node version', /10\.\d+\.\d+/)
-      if (ok) this.couldContains('a recent lts node version', /v12\.16\.\d+/)
-    }
+    this.couldContains('a recent lts node version', /14\.\d+\.\d+/)
   }
 }
