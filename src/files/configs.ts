2d391cf3fe2127e7d9b1c139fafdd332a7493b75
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-01-03 13:15:33 +0100

    dont suggests css comb anymore

--- src/files/configs.ts
+++ src/files/configs.ts
@@ -3,6 +3,5 @@ import { File } from '../file'
 export class ConfigsFile extends File {
   async start (): Promise<void> {
     await this.checkFileExists('.gitignore')
-    if (this.data.web_published) await this.checkFileExists('.csscomb.json', true)
   }
 }
