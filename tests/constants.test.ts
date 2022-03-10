455e97e555c95b88ba30fa49004b17ea8b8b3be8
Romain Racamier-Lafon <romain.racamier@gmail.com>
2022-03-10 09:14:38 -0800

    enhance coverage

--- tests/constants.test.ts
+++ tests/constants.test.ts
@@ -1,6 +1,6 @@
 import { strictEqual as equal } from 'assert'
 import { test } from 'uvu'
-import { home, repoCheckerPath } from '../src/constants'
+import { home, ProjectData, repoCheckerPath } from '../src/constants'
 
 test('home (process.env.HOME) is defined', function () {
   equal(home.length > 0, true)
@@ -10,6 +10,16 @@ test('home (process.env.HOME) is defined', function () {
 test('repoCheckerPath (process.env.pwd) is defined', function () {
   equal(repoCheckerPath.length > 0, true)
   // console.log(`repoCheckerPath is "${repoCheckerPath}"`)
+})
+
+test('ProjectData ban sass by default', function (){
+  const data = new ProjectData()
+  equal(data.ban_sass, true)
+})
+
+test('ProjectData assign', function (){
+  const data = new ProjectData({ ban_sass: false })
+  equal(data.ban_sass, false)
 })
 
 test.run()
