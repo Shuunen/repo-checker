3af5519dfe6179953695279fd355b8b8bd967911
Romain Racamier-Lafon <romain.racamier@gmail.com>
2022-03-10 09:50:45 +0100

    check for watchlist eager, fix #312

--- src/files/package.ts
+++ src/files/package.ts
@@ -57,6 +57,7 @@ export class PackageJsonFile extends File {
     else this.couldContains('a post-script for version automation', /"postversion": "/, 1, 'like : "postversion": "git push && git push --tags",')
     if (this.fileContent.includes('"prepublish"')) this.shouldContains('"prepare" instead of "prepublish" (deprecated)', /"prepublish"/, 0)
     if (this.data.use_typescript) this.shouldContains('a typescript build or check', /(tsc)|(tsc --noEmit)/)
+    if (this.fileContent.includes('watchlist')) this.couldContains('watchlist eager param', /-eager --/, 1, 'like watchlist src tests -eager -- npm run test')
     if (!this.fileContent.includes('github.com/Shuunen')) return
     if (this.data.package_name !== 'repo-check') this.couldContains('a repo-check script', /"check": "repo-check"/, 1, '(don\'t forget to npm i repo-check)')
     this.couldContains('a ci script', /"ci": "/, 1, 'like "ci": "run-s build lint check test" (don\'t forget to npm i npm-run-all)')
