5733b24b26706bf5d5ba08dcfe4c029ef2b173b3
Romain Racamier-Lafon <romain.racamier@gmail.com>
2022-03-23 23:28:51 +0100

    add checks for eslintrc custom rules

--- src/files/eslint.ts
+++ src/files/eslint.ts
@@ -1,5 +1,15 @@
+import { isJSON } from 'shuutils'
+import { repoCheckerPath } from '../constants'
 import { File } from '../file'
 import { log } from '../logger'
+import { readFileInFolder } from '../utils'
+
+class EslintRcJsonFile {
+  rules: Record<string, string | string[]> = {}
+  constructor (data: Partial<EslintRcJsonFile> = {}) {
+    Object.assign(this, data)
+  }
+}
 
 export class EsLintFile extends File {
   async start (): Promise<boolean> {
@@ -14,10 +24,28 @@ export class EsLintFile extends File {
     await this.inspectFile(filename)
     this.couldContains('eslint recommended rules extend', /"eslint:recommended"/)
     this.couldContains('unicorn rules extend', /plugin:unicorn\/recommended/)
+    this.couldContains('unicorn plugin', /"unicorn"/)
     this.shouldContains('no promise plugin (require eslint 7)', /(plugin:promise\/recommended)|("promise")/, 0)
+    await this.checkRules()
     if (this.data.use_vue) await this.checkVue()
     if (this.data.use_typescript) return this.checkTs()
     return this.checkJs()
+  }
+
+  async checkRules (): Promise<boolean> {
+    const json = isJSON(this.fileContent)
+    if (typeof json === 'boolean') return log.warn('cannot check empty or invalid .eslintrc.json file')
+    const rules = new EslintRcJsonFile(json).rules
+    const expectedJson = await readFileInFolder(repoCheckerPath, '.eslintrc.json')
+    const expectedRules = new EslintRcJsonFile(JSON.parse(expectedJson)).rules
+    const missingRules = Object.keys(expectedRules).filter(rule => {
+      if (rule.startsWith('@typescript') && !this.data.use_typescript) return false
+      return !(rule in rules)
+    })
+    const total = Object.keys(expectedRules).length
+    const ok = this.test(missingRules.length === 0, `current .eslintrc.json has only ${total - missingRules.length} of the ${total} custom rules in repo-checker .eslintrc.json`, true)
+    if (!ok) log.warn('missing rules :', missingRules.map(rule => `"${rule}": ${JSON.stringify(expectedRules[rule])}`).join(', '))
+    return true
   }
 
   async checkJs (): Promise<boolean> {
--- tests/files/eslint.test.ts
+++ tests/files/eslint.test.ts
@@ -28,17 +28,94 @@ test('eslint config file empty', async function () {
   equal(instance.nbFailed, 2)
 })
 
-test('eslint vue ts config file empty', async function () {
+test('eslint config file empty for vue ts project', async function () {
   log.consoleLog = false
   log.fileLog = false
-  const instance = new EsLintFile('', new ProjectData({ use_vue:true, use_typescript: true, quiet: true }), true)
+  const instance = new EsLintFile('', new ProjectData({ use_vue: true, use_typescript: true }))
   instance.fileExists = async () => true
   instance.inspectFile = async () => void 0
   instance.fileContent = ''
   await instance.start()
   await instance.end()
   equal(instance.nbPassed, 2)
-  equal(instance.nbFailed, 5)
+  equal(instance.nbFailed, 7)
+})
+
+test('eslint config partial file for js project', async function () {
+  log.consoleLog = false
+  log.fileLog = false
+  const instance = new EsLintFile()
+  instance.fileExists = async () => true
+  instance.inspectFile = async () => void 0
+  instance.fileContent = `{
+    "extends": [
+      "eslint:recommended",
+      "plugin:unicorn/recommended"
+    ],
+    "plugins": [
+      "unicorn"
+    ],
+    "rules": {}
+  }`
+  await instance.start()
+  await instance.end()
+  equal(instance.nbPassed, 5)
+  equal(instance.nbFailed, 1)
+})
+
+test('eslint config partial file for ts project', async function () {
+  log.consoleLog = false
+  log.fileLog = false
+  const instance = new EsLintFile('', new ProjectData({ use_typescript: true }))
+  instance.fileExists = async () => true
+  instance.inspectFile = async () => void 0
+  instance.fileContent = `{
+    "extends": [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:unicorn/recommended"
+    ],
+    "plugins": [
+      "@typescript-eslint",
+      "unicorn"
+    ],
+    "rules": {}
+  }`
+  await instance.start()
+  await instance.end()
+  equal(instance.nbPassed, 6)
+  equal(instance.nbFailed, 1)
+})
+
+test('eslint config partial file for vue ts project', async function () {
+  log.consoleLog = false
+  log.fileLog = false
+  const instance = new EsLintFile('', new ProjectData({ use_vue: true, use_typescript: true }))
+  instance.fileExists = async () => true
+  instance.inspectFile = async () => void 0
+  instance.fileContent = `{
+    "extends": [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:unicorn/recommended",
+      "plugin:vue/vue3-recommended"
+    ],
+    "parser": "vue-eslint-parser",
+    "parserOptions": {
+      "parser": "@typescript-eslint/parser",
+      "ecmaVersion": 2021,
+      "sourceType": "module"
+    },
+    "plugins": [
+      "@typescript-eslint",
+      "unicorn"
+    ],
+    "rules": {}
+  }`
+  await instance.start()
+  await instance.end()
+  equal(instance.nbPassed, 11)
+  equal(instance.nbFailed, 1)
 })
 
 test.run()
