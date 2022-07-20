3c934c97e04c87c86553d448a749f8b9655ba037
Romain Racamier-Lafon <romain.racamier@gmail.com>
2022-07-20 20:20:42 +0200

    stack upgrade

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@
 import { log } from './logger'
 import { start } from './main'
 
+// eslint-disable-next-line unicorn/prefer-top-level-await
 start().catch(error => {
   log.error(error.message)
   log.line()
