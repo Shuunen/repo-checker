669639b0f821ff3922be7d076a0225446b985596
Romain Racamier-Lafon <romain.racamier@gmail.com>
2022-10-27 23:15:44 +0200

    bump node lts

--- src/files/nvmrc.ts
+++ src/files/nvmrc.ts
@@ -6,7 +6,7 @@ export class NvmrcFile extends File {
     const exists = await this.checkFileExists('.nvmrc')
     if (!exists) return
     await this.inspectFile('.nvmrc')
-    this.couldContains('a recent lts node version', /1(4|6)\.\d+\.\d+/)
+    this.couldContains('a recent lts node version', /1(6|8)\.\d+\.\d+/)
   }
 }
 /* c8 ignore stop */
