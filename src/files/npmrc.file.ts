50efff47a726ef52efdfd32f7628789aba731f4d
Romain Racamier-Lafon <romain.racamier@gmail.com>
2023-04-09 18:32:37 +0200

    make npmrc fixable

--- src/files/npmrc.file.ts
+++ src/files/npmrc.file.ts
@@ -2,11 +2,12 @@ import { FileBase } from '../file'
 
 export class NpmRcFile extends FileBase {
   public async start (): Promise<void> {
-    const hasFile = await this.fileExists('.npmrc')
-    this.test(this.data.hasTaskPrefix && hasFile, '.npmrc file should be present when using pre/post task in package.json to set enable-pre-post-scripts option')
+    if (!this.data.hasTaskPrefix) return
+    const hasFile = await this.checkFileExists('.npmrc', true)
+    this.test(hasFile, '.npmrc is present (needed when using pre/post task in package.json, need to set enable-pre-post-scripts option)', true)
     if (!hasFile) return
     await this.inspectFile('.npmrc')
-    this.shouldContains('enable-pre-post-scripts option', /enable-pre-post-scripts=true/u)
+    this.couldContains('enable-pre-post-scripts option', /enable-pre-post-scripts=true/u)
   }
 }
 
