cffd1a584f619c15bb932ad6429b920054dc7762
Romain Racamier-Lafon <romain.racamier@gmail.com>
2023-07-17 00:04:06 +0200

    stop enforcing strict-peer-dependencies

--- src/files/npmrc.file.ts
+++ src/files/npmrc.file.ts
@@ -8,18 +8,11 @@ export class NpmRcFile extends FileBase {
     if (!isOk && this.canFix) this.fileContent += '\nenable-pre-post-scripts=true'
   }
 
-  private checkStrictPeerDependencies () {
-    const isOk = this.couldContains('no strict-peer-dependencies option', /strict-peer-dependencies=false/u, 0, undefined, true)
-    /* c8 ignore next */
-    if (!isOk && this.canFix) this.fileContent = this.fileContent.replace('\nstrict-peer-dependencies=false', '')
-  }
-
   public async start (): Promise<void> {
     const hasFile = await this.checkFileExists('.npmrc', true)
     if (!hasFile) return
     await this.inspectFile('.npmrc')
     this.checkPrePost()
-    this.checkStrictPeerDependencies()
   }
 
 }
