81b5a5b08826c9c2f44cc07b82204bbc949e953a
Romain Racamier-Lafon <romain.racamier@gmail.com>
2024-10-26 17:20:17 +0200

    bump biome

--- src/mocks/tsProject/tests/some.ts
+++ src/mocks/tsProject/tests/some.ts
@@ -2,4 +2,5 @@
 // biome-ignore lint/style/useNodejsImportProtocol: let me live my life
 import assert from 'node:assert/strict'
 
+// biome-ignore lint/suspicious/noMisplacedAssertion: it's ok buddy
 assert.equal(1, 1)
