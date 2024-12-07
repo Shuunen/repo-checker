475195329d6a8866e8693e37b5406d673beda246
Romain Racamier-Lafon <romain.racamier@gmail.com>
2024-12-07 12:00:09 +0100

    chore(deps): update Node.js version to 22.12.0 in .nvmrc and related files

--- src/files/nvmrc.ts
+++ src/files/nvmrc.ts
@@ -10,7 +10,7 @@ export class NvmrcFile extends FileBase {
     const hasFile = await this.checkFileExists('.nvmrc')
     if (!hasFile) return
     await this.inspectFile('.nvmrc')
-    this.couldContains('a recent lts node version', /20\.\d+\.\d+/u)
+    this.couldContains('a recent lts node version', /22\.\d+\.\d+/u)
   }
 }
 /* c8 ignore stop */
