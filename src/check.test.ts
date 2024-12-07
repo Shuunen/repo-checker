dcb1eff55e88f50a8af39ff5d78895f862eb54f7
Romain Racamier-Lafon <romain.racamier@gmail.com>
2024-12-07 12:00:31 +0100

    refactor(tests): rename test cases and snapshots for clarity

--- src/check.test.ts
+++ src/check.test.ts
@@ -3,33 +3,33 @@ import { check } from './check'
 import { ProjectData, repoCheckerPath } from './constants'
 import { mocksProjectsFolder, tsProjectFolder } from './mock'
 
-it('check A folder fails with low max size', async () => {
+it('check A repo-checker folder fails with low max size', async () => {
   const data = new ProjectData({ isPublishedPackage: true, isQuiet: true, maxSizeKo: 2 })
   // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
   const message = await check({ data, folderPath: repoCheckerPath }).catch((error: unknown) => (error as Error).message)
   expect(message).toMatchSnapshot()
 })
 
-it('check B folder succeed', async () => {
+it('check B repo-checker folder succeed', async () => {
   const data = new ProjectData({ isModule: true, isPublishedPackage: true, isQuiet: true, maxSizeKo: 120 })
   const indicators = await check({ data, folderPath: repoCheckerPath })
   expect(indicators).toMatchSnapshot()
 })
 
-it('check C data/tsProject', async () => {
+it('check C ts-project folder', async () => {
   const data = new ProjectData({ isQuiet: true })
   const indicators = await check({ canThrow: false, data, folderPath: tsProjectFolder })
   expect(indicators).toMatchSnapshot()
 })
 
-it('check D data folders and throw', async () => {
+it('check D mocks-projects folders and throw', async () => {
   const data = new ProjectData({ isQuiet: true })
   // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
   const message = await check({ canFailStop: true, data, folderPath: mocksProjectsFolder }).catch((error: unknown) => (error as Error).message)
   expect(message).toMatchSnapshot()
 })
 
-it('check E data folders and not throw', async () => {
+it('check E mocks-projects folders and not throw', async () => {
   const data = new ProjectData({ isQuiet: true })
   const indicators = await check({ canFailStop: true, canThrow: false, data, folderPath: mocksProjectsFolder })
   expect(indicators).toMatchSnapshot()
