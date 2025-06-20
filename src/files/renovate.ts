fa9a34e87f9894c62c55b2ce56e19c3d9d250b38
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-06-20 11:13:48 +0200

    feat: update renovate configuration

--- src/files/renovate.ts
+++ src/files/renovate.ts
@@ -10,11 +10,9 @@ export class RenovateFile extends FileBase {
     await this.inspectFile('renovate.json')
     this.couldContainsSchema('https://docs.renovatebot.com/renovate-schema.json')
     this.shouldContains('an extends section', /"extends"/u)
-    this.shouldContains('a base config', /"config:base"/u)
+    this.shouldContains('no deprecated base config, use config:semverAllMonthly instead', /"config:base"/u, 0)
     this.shouldContains('a dashboard setting to false', /"dependencyDashboard": false/u)
     if (this.data.canAutoMergeDeps) this.shouldContains('an auto merge preset', /":automergeAll"/u)
-    const hasPreserveSemver = this.shouldContains('a preserve semver ranges preset', /":preserveSemverRanges"/u, 1, true, undefined, true)
-    if (!hasPreserveSemver && this.canFix) this.fileContent = this.fileContent.replace('":automergeAll"', '":automergeAll",\n    ":preserveSemverRanges"')
   }
 }
 /* c8 ignore stop */
